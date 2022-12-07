629feca1ed2f2eea4ec6466c8c1e5bb17118bb3f
Blair D. Patterson <tentwofour@users.noreply.github.com>
2022-12-07 14:05:39 -0600

    Update UnitOfWork.php
    Line 2665 missed continue => continue 2 for php7.4+ compatibility

--- lib/Doctrine/ORM/UnitOfWork.php
+++ lib/Doctrine/ORM/UnitOfWork.php
@@ -2662,7 +2662,7 @@ class UnitOfWork implements PropertyChangedListener
                         $class->reflFields[$field]->setValue($entity, null);
                         $this->originalEntityData[$oid][$field] = null;
 
-                        continue;
+                        continue 2;
                     }
 
                     if ( ! isset($hints['fetchMode'][$class->name][$field])) {
